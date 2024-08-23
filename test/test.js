79f4d6bbeb29bf6e18d11afe130cbf3043fd78e8
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2024-08-23 20:37:43 +0700

    menambahkan ML dan IoT

---
+++ test/test.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const { performance } = require("perf_hooks");
+
+async function fetch(url, data) {
+  try {
+    const response = await axios.post(url, data);
+    return { status: response.status, data: response.data };
+  } catch (error) {
+    return { status: null, error: error.message };
+  }
+}
+
+async function main(url, numRequests, data) {
+  const requests = [];
+
+  // Mulai pengukuran waktu
+  const startTime = performance.now();
+
+  for (let i = 0; i < numRequests; i++) {
+    requests.push(fetch(url, data));
+  }
+
+  await Promise.all(requests);
+
+  // Akhir pengukuran waktu
+  const endTime = performance.now();
+
+  // Hitung dan tampilkan waktu total dalam detik
+  const totalTime = (endTime - startTime) / 1000; // Konversi milidetik ke detik
+  console.log(`Total time taken: ${totalTime.toFixed(4)} seconds`);
+}
+
+const targetUrl = "http://localhost:3100/posisi/predict"; // Ganti dengan URL lokal server Anda
+const numberOfRequests = 100; // Jumlah request yang ingin dikirimkan
+const bodyData = {
+  lat_naik: -6.9201824,
+  lon_naik: 107.6216454,
+  jurusan: "Cibiru - Leuwipanjang",
+};
+
+main(targetUrl, numberOfRequests, bodyData);
